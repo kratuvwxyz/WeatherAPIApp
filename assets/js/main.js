3b99909f5967632330cdce0e40bd75f21110dd46
Kratu Desai <saibook@Kratus-MacBook-Pro.local>
2020-02-18 01:17:32 -0600

    add date

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,6 @@
+// time
+$('.time').text(moment().format('dddd, MMMM Do YYYY'));
+
 // Weather Api
 const apiKey = "aed52c01e7f5375831def9553ce0837d";
 // https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}
@@ -15,29 +18,29 @@ if (cityName2 === null){
 }
 
 $(document).ready(function(){
+    // create new button
     let createNewButn = function(){
         $('.cityNames').empty();
         for(let i = 0; i < cityName.length; i++){
             let newCityButton = $('<button>').addClass('searchBtn btn btn-dark btn-lg btn-block').text(cityName[i]).val(i);
-            let cancel = $('<button>').addClass('remove btn btn-light btn-sm float-right').text('x');
+            let cancel = $('<button>').addClass('remove btn btn-dark btn-sm float-right').text('X');
             newCityButton.append(cancel);
             $('.cityNames').append(newCityButton).addClass('pr-5');
         }
     }
     createNewButn();
-    
+    // remove button
     $(document).on('click', '.remove', function(){
         let value = $(this).closest('.searchBtn').val();
         cityName.splice(value, 1);
         localStorage.setItem("myCityKey", JSON.stringify(cityName));
         $(this).closest('.searchBtn').remove();
     });
-
+    // click search button to create new button
     $(document).on('click', '#searchBtn', function(){
         let city = $("#city").val();
         $("#city").val("");
         cityName.push(city);console.log(cityName);
-        
         localStorage.setItem("myCityKey", JSON.stringify(cityName));
         createNewButn();
     });
