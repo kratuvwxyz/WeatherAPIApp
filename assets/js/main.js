8793b9248074f05db63303a7f2fbbc0eb62542e2
Kratu Desai <saibook@Kratus-MacBook-Pro.local>
2020-02-18 01:02:24 -0600

    New button , Remove button and add search result

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,17 +2,23 @@
 const apiKey = "aed52c01e7f5375831def9553ce0837d";
 // https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}
 
-let cityName = [];
+let cityName = [];console.log(cityName);
 
 let retrievedData = localStorage.getItem("myCityKey");console.log(retrievedData);
 
 let cityName2 = JSON.parse(retrievedData);console.log(cityName2);
 
+if (cityName2 === null){
+    cityName = ["Austin", "Houston", "Dallas", "San Antonio"];
+} else {
+    cityName = cityName2;console.log(cityName);
+}
 
 $(document).ready(function(){
     let createNewButn = function(){
-        for(let i = 0; i < cityName2.length; i++){
-            let newCityButton = $('<button>').addClass('searchBtn btn btn-dark btn-lg btn-block').text(cityName2[i]).val(cityName2[i]);
+        $('.cityNames').empty();
+        for(let i = 0; i < cityName.length; i++){
+            let newCityButton = $('<button>').addClass('searchBtn btn btn-dark btn-lg btn-block').text(cityName[i]).val(i);
             let cancel = $('<button>').addClass('remove btn btn-light btn-sm float-right').text('x');
             newCityButton.append(cancel);
             $('.cityNames').append(newCityButton).addClass('pr-5');
@@ -20,11 +26,20 @@ $(document).ready(function(){
     }
     createNewButn();
     
+    $(document).on('click', '.remove', function(){
+        let value = $(this).closest('.searchBtn').val();
+        cityName.splice(value, 1);
+        localStorage.setItem("myCityKey", JSON.stringify(cityName));
+        $(this).closest('.searchBtn').remove();
+    });
+
     $(document).on('click', '#searchBtn', function(){
         let city = $("#city").val();
         $("#city").val("");
-        cityName.push(city);
+        cityName.push(city);console.log(cityName);
+        
         localStorage.setItem("myCityKey", JSON.stringify(cityName));
+        createNewButn();
     });
 
 });
