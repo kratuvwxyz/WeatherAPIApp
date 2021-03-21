38efae40d65a8d718bec8e430e46a881d28328c1
kratuvwxyz <sai@desaign.me>
2021-03-21 09:56:01 -0500

    error code updated

--- script.js
+++ script.js
@@ -11,8 +11,14 @@ dataSetup = (x) => {
   $(".lat").text(x.coord.lat);
   $(".lon").text(x.coord.lon);
   $(".country").text(x.sys.country);
-  $(".weatherIcon").attr("src", `http://openweathermap.org/img/wn/${x.weather[0].icon}@2x.png`);
-  $(".temp").html(Math.round((x.main.temp - 273.15) * 1.8 + 32) + "<sup style='font-size:.5em;'>˚F</sup>");
+  $(".weatherIcon").attr(
+    "src",
+    `http://openweathermap.org/img/wn/${x.weather[0].icon}@2x.png`
+  );
+  $(".temp").html(
+    Math.round((x.main.temp - 273.15) * 1.8 + 32) +
+      "<sup style='font-size:.5em;'>˚F</sup>"
+  );
   $(".feelsLike").html(Math.round((x.main.feels_like - 273.15) * 1.8 + 32));
   $(".lowTemp").text(Math.round((x.main.temp_min - 273.15) * 1.8 + 32));
   $(".highTemp").text(Math.round((x.main.temp_max - 273.15) * 1.8 + 32));
@@ -42,13 +48,17 @@ fourDaysDataSetup = (x) => {
               <div class="col m-3">
                 <p class="fs-4" style="line-height: auto !important"><span class="four-day">${xdate}</span><br/><strong class="four-date fs-1">${xydate}</strong></p>
               </div>
-              <div class="col"><img src="http://openweathermap.org/img/wn/${y.weather[0].icon}@2x.png" class="four-image" alt="Weather Icon" width="100%" /></div>
+              <div class="col"><img src="http://openweathermap.org/img/wn/${
+                y.weather[0].icon
+              }@2x.png" class="four-image" alt="Weather Icon" width="100%" /></div>
             </div>
           </div>
           <div class="col-8 col-sm-4 col-md-12 p-1 text-center d-flex align-self-center justify-content-center">
             <div class="row p-0">
               <div class="col py-3">
-                <span class="four-fs-10">${Math.round((y.main.temp - 273.15) * 1.8 + 32)}<sup class="fs-11">˚F</sup></span>
+                <span class="four-fs-10">${Math.round(
+                  (y.main.temp - 273.15) * 1.8 + 32
+                )}<sup class="fs-11">˚F</sup></span>
                 <p class="fs-6">${y.weather[0].description}</p>
               </div>
             </div>
@@ -56,12 +66,20 @@ fourDaysDataSetup = (x) => {
           <div class="col-6 d-none d-sm-block col-md-12 text-center under-375" style="letter-spacing: 0">
             <div class="row">
               <div class="col py-3">
-                <sup><i class="fas text-secondary fa-temperature-low fs-12"></i></sup>&#160;<span class="fs-11">${Math.round((y.main.temp_min - 273.15) * 1.8 + 32)}</span><sup class="text-primary x fs-12">˚F</sup>&#160;&#160;&#160;<sup><i class="fas text-secondary fa-temperature-high fs-12"></i></sup>&#160;<span class="fs-11">${Math.round((y.main.temp_max - 273.15) * 1.8 + 32)}</span><sup class="text-primary x fs-12">˚F</sup>
+                <sup><i class="fas text-secondary fa-temperature-low fs-12"></i></sup>&#160;<span class="fs-11">${Math.round(
+                  (y.main.temp_min - 273.15) * 1.8 + 32
+                )}</span><sup class="text-primary x fs-12">˚F</sup>&#160;&#160;&#160;<sup><i class="fas text-secondary fa-temperature-high fs-12"></i></sup>&#160;<span class="fs-11">${Math.round(
+      (y.main.temp_max - 273.15) * 1.8 + 32
+    )}</span><sup class="text-primary x fs-12">˚F</sup>
               </div>
             </div>
             <div class="row">
               <div class="col py-3">
-                <sup><i class="far text-secondary fa-sun fs-12"></i></sup>&#160;<span class="fs-11">${y.main.temp_kf}</span><sup class="text-primary x fs-12">UV</sup>&#160;&#160;&#160;<sup><i class="fas text-secondary fa-tint fs-12"></i></sup>&#160;<span class="fs-11">${y.main.humidity}</span><sup class="text-primary x fs-12">%</sup>
+                <sup><i class="far text-secondary fa-sun fs-12"></i></sup>&#160;<span class="fs-11">${
+                  y.main.temp_kf
+                }</span><sup class="text-primary x fs-12">UV</sup>&#160;&#160;&#160;<sup><i class="fas text-secondary fa-tint fs-12"></i></sup>&#160;<span class="fs-11">${
+      y.main.humidity
+    }</span><sup class="text-primary x fs-12">%</sup>
               </div>
             </div>
           </div>
@@ -104,40 +122,26 @@ $(document).ready(() => {
       x.innerHTML = "Geolocation is not supported by this browser.";
     }
   }
-
-  function showPosition(position) {
-    $.ajax({
-      url: `https://api.openweathermap.org/data/2.5/forecast?appid=${apiKey}&lat=${position.coords.latitude}&lon=${position.coords.longitude}`,
-      method: "GET",
-    }).then((response) => {
-      fourDaysDataSetup(response);
-    });
-    $.ajax({
-      url: `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&lat=${position.coords.latitude}&lon=${position.coords.longitude}`,
-      method: "GET",
-    }).then((response) => {
-      dataSetup(response);
-    });
-  }
-  getLocation();
-  function showError(error) {
-    switch (error.code) {
-      case error.PERMISSION_DENIED:
+  navigator.geolocation.watchPosition(
+    function (position) {
+      $.ajax({
+        url: `https://api.openweathermap.org/data/2.5/forecast?appid=${apiKey}&lat=${position.coords.latitude}&lon=${position.coords.longitude}`,
+        method: "GET",
+      }).then((response) => {
+        fourDaysDataSetup(response);
+      });
+      $.ajax({
+        url: `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&lat=${position.coords.latitude}&lon=${position.coords.longitude}`,
+        method: "GET",
+      }).then((response) => {
+        dataSetup(response);
+      });
+    },
+    function (error) {
+      if (error.code == error.PERMISSION_DENIED || error.code == error.POSITION_UNAVAILABLE || error.code == error.TIMEOUT || error.code == error.UNKNOWN_ERROR) {
         newWeather("Austin");
         newForcast("Austin");
-        break;
-      case error.POSITION_UNAVAILABLE:
-        newWeather("Austin");
-        newForcast("Austin");
-        break;
-      case error.TIMEOUT:
-        newWeather("Austin");
-        newForcast("Austin");
-        break;
-      case error.UNKNOWN_ERROR:
-        newWeather("Austin");
-        newForcast("Austin");
-        break;
+      }
     }
-  }
+  );
 });
